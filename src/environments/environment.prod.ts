55622db60e1aeee3635903e63dd08323a50ff38e
SamySalazar <samsalazarleal@gmail.com>
2022-05-25 16:51:53 -0500

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'instapirate-xd',
+    appId: '1:671776713314:web:a3ed8e21db8ab00fd6bf3b',
+    databaseURL: 'https://instapirate-xd-default-rtdb.firebaseio.com',
+    storageBucket: 'instapirate-xd.appspot.com',
+    apiKey: 'AIzaSyCZduPivYcQ66fh0MkQon94h4THI1bmUVU',
+    authDomain: 'instapirate-xd.firebaseapp.com',
+    messagingSenderId: '671776713314',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'instapirate-xd',
+    appId: '1:671776713314:web:a3ed8e21db8ab00fd6bf3b',
+    databaseURL: 'https://instapirate-xd-default-rtdb.firebaseio.com',
+    storageBucket: 'instapirate-xd.appspot.com',
+    apiKey: 'AIzaSyCZduPivYcQ66fh0MkQon94h4THI1bmUVU',
+    authDomain: 'instapirate-xd.firebaseapp.com',
+    messagingSenderId: '671776713314',
+  }
 };
 
 /*
